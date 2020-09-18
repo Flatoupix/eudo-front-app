17ceb9966c3a3f38c0f6fdc603e3ba0eecdbcd59
Kaïs Jemai <kjemai@eudonet.com>
2020-09-18 09:22:38 +0200

    ajout  tooltip dans edn-memo

--- src/components/ednMemo.vue
+++ src/components/ednMemo.vue
@@ -25,7 +25,17 @@
       :ename="$attrs.ename"
       :id="$attrs.id"
     >
+
+    <template v-slot:append v-if="$attrs.tooltip">
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-icon v-on="on" style="cursor:pointer;">mdi-help-circle-outline</v-icon>
+          </template>
+          {{ $attrs.tooltip }}
+        </v-tooltip>
+      </template>
     </v-textarea>
+    
   </div>
 </template>
 
