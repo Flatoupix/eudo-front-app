1b171ae5bdebcfb1fc41141312269f548b2f695f
Quentin BOLATRE <bolatre.quentin@gmail.com>
2021-02-23 17:00:59 +0100

    Ajout d'un switch en fonction des type de component dans EntryFields

--- src/features/components/EntryFields.vue
+++ src/features/components/EntryFields.vue
@@ -1,29 +1,37 @@
 <template>
   <!-- Debut Entry Fields -->
-  <v-card elevation="0">
+  <v-card flat>
     <!-- Debut title -->
-    <v-card-title>{{entryFieldsContent.title}}</v-card-title>
+    <v-card-title>{{ entryFieldsContent.title }}</v-card-title>
     <!-- Fin title -->
 
     <!-- Debut subtitle -->
-    <v-card-subtitle>{{entryFieldsContent.description}}</v-card-subtitle>
+    <v-card-subtitle>{{ entryFieldsContent.description }}</v-card-subtitle>
     <!-- Fin subtitle -->
 
     <!-- Debut content -->
     <v-card-text>
       <v-form ref="form">
-        <ednField :label="field.Label" v-for="field in entryFieldsContent.fields" :key="field.id"></ednField>
+        <component
+          v-for="field in entryFieldsContent.fields"
+          irisMimic
+          :label="getLabel(field)"
+          :key="field.id"
+          :is="dynamicFormatChamps(field)"
+        ></component>
       </v-form>
     </v-card-text>
     <!-- fIN content -->
-
   </v-card>
   <!-- Fin Entry Fields -->
 </template>
 <script>
 import ednField from '../../components/ednField.vue'
+import ednPhone from '../../components/ednPhone.vue'
+import ednCheck from '../../components/ednCheck.vue'
+import ednCat from '../../components/ednCat.vue'
 export default {
-  components: { ednField },
+  components: { ednField, ednCheck, ednCat,ednPhone},
   name: 'EntryFieldsComponent',
   data() {
     return {}
@@ -34,7 +42,23 @@ export default {
       default: () => {},
     },
   },
-  methods: {},
+  methods: {
+    getLabel(field) {
+      return field.LabelHidden ? '' : field.Label ? field.Label : ''
+    },
+    dynamicFormatChamps(input) {
+      switch (input.Format) {
+        case 6:
+          return ednField
+        case 11:
+          return ednCheck
+        case 25:
+          return ednCat
+        default:
+          return ednField
+      }
+    },
+  },
   mounted() {},
 }
 </script>
